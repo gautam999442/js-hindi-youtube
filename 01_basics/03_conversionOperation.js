0d3b01f5b607f158788a3e06e48b97535e77bdd4
gautam999442 <164536341+gautam999442@users.noreply.github.com>
2024-04-28 22:02:18 +0530

    changin in 03 file and and new file add

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,58 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// ****************************oprations******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2%4)
+// console.log(2-2)
+
+let Str1 = "hello"
+let Str2 = " himanshu"
+
+let str3 = Str1 + Str2
+
+console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(+true)
+// console.log(true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+//  console.log(num2) 
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
