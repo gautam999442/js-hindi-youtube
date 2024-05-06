c01d0d92f47137e42238f704ba9741729444d346
gautam999442 <164536341+gautam999442@users.noreply.github.com>
2024-05-06 18:02:14 +0530

    0_6_num_and_Math.js

---
+++ 01_basics/06_num_and_Math.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score);
+
+const Balance = new Number(100) 
+// console.log(Balance);
+// console.log(Balance.toString().length);
+
+// console.log(Balance.toFixed(2))
+
+const otherNumber = 1123.89645
+// console.log(otherNumber.toPrecision(3));
+
+const Hunderd = 100000
+// console.log(Hunderd.toLocaleString( 'en-IN' )) 
+// +++++++++++++++++++++++++++++++++++++Math+++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(2, 12, 8, 25));
+// console.log(Math.max(2, 12, 8, 25));
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) +min)
