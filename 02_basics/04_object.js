ca952814195bb5b5f21c435fd528662302a887f1
gautam999442 <164536341+gautam999442@users.noreply.github.com>
2024-05-24 13:55:38 +0530

    json

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -47,8 +47,42 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('name'));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('name'));
+
+
+// object have name like  course
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh",
+}
+  
+// console.log(course.courseInstructor); normal process 
+
+const {coursename : user} = course
+
+// console.log(user)
+
+// const navbar = ({ company}) => {
+
+// }  
+
+// navbar(company = "hitesh")
+
+// json   it has no name it started with {} curly breses keys and value both are strings
+
+// {
+//     "name": "himasnhu"
+//     "coursename": "js in hindi"
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
