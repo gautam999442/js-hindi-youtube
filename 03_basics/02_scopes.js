f9e4f06709f3294d7952e014644529ae4ed247ab
gautam999442 <164536341+gautam999442@users.noreply.github.com>
2024-05-29 15:28:46 +0530

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,51 @@
+//var c = 300
+let a = 300
+
+if(true){
+
+    let a = 10
+    const b = 20 
+    // console.log("INNER:",a);
+
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "himanshu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true){
+    const username = "himanshu"
+    if (username === "himanshu"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    // console.log(username);
+}
+// console.log(username);
+
+//+++++++++++++++++++++ interesting +++++++++++++++++++
+
+console.log(addonen(5))
+
+function addonen(num) {
+    return num + 1
+}
+
+(addTwo(5))
+
+const addTwo = function(num){
+    return num + 2
+}
