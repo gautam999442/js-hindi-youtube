55b23d4a1ec7476a592748535ea7af7c54b4d101
gautam999442 <164536341+gautam999442@users.noreply.github.com>
2024-06-11 12:46:31 +0530

    aadded controlflow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+//  switch (key) {
+//     case value:
+        
+//         break;
+ 
+//     default:
+//         break;
+//  }
+
+const month = 1
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("fabuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("aprial");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
